5f04458e7c8db617800ae38e1eaa3b93c8cec688
sandrocuttos <sandrocuttos@outlook.com>
2022-08-04 03:32:03 -0400

    Update DineOrderService.java

--- src/main/java/com/project/dinein/service/DineOrderService.java
+++ src/main/java/com/project/dinein/service/DineOrderService.java
@@ -2,8 +2,11 @@ package com.project.dinein.service;
 
 import com.project.dinein.dao.DineOrderDAO;
 import com.project.dinein.model.DineOrder;
+import com.project.dinein.model.Dish;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DineOrderService {
@@ -21,5 +24,11 @@ public class DineOrderService {
     public int createOrder (DineOrder order){
 
         return dineOrderDAO.insertOrder(order);
+    }
+
+    public List<Dish> getAllDishes (DineOrder order){
+
+
+
     }
 }
